391270d8b29a70f97f9fd3db76b00e88cc8dbeeb
Mike Simms <msimms01@gmail.com>
2022-08-18 12:45:44 -0400

    Initial checkin.

---
+++ Hydrometer/Hydrometer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Hydrometer
+//
+//  Created by Michael Simms on 8/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+		Text("Temperature")
+			.padding()
+		Text("Gravity")
+			.padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hydrometer/Hydrometer/HydrometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrometerApp.swift
+//  Hydrometer
+//
+//  Created by Michael Simms on 8/18/22.
+//
+
+import SwiftUI
+
+@main
+struct HydrometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
