dbd06c106449a7802e11922149449bc437fe478a
Mike Simms <msimms01@gmail.com>
2022-08-18 15:18:11 -0400

    Added plist.

--- Hydrometer/Hydrometer/HydrometerApp.swift
+++ Hydrometer/Hydrometer/HydrometerApp.swift
@@ -6,10 +6,43 @@
 //
 
 import SwiftUI
+import CoreBluetooth
+
+class AppState : ObservableObject {
+	
+	static let shared = AppState()
+	@Published var currentTemp: Float = 0
+	@Published var currentGravity: Float = 0
+	
+	/// Called when a peripheral is discovered.
+	/// Returns true to indicate that we should connect to this peripheral and discover its services.
+	func peripheralDiscovered(description: String) -> Bool {
+		return true
+	}
+	
+	/// Called when a service is discovered.
+	func serviceDiscovered(serviceId: CBUUID) {
+	}
+	
+	/// Called when a sensor characteristic is updated.
+	func valueUpdated(peripheral: CBPeripheral, serviceId: CBUUID, value: Data) {
+	}
+	
+	func startBluetoothScanning() -> BluetoothScanner {
+		let scanner = BluetoothScanner()
+		let interestingServices = [CBUUID(data: CUSTOM_BT_SERVICE_TILT_HYDROMETER)]
+		
+		// Start scanning for the services that we are interested in.
+		scanner.startScanning(serviceIdsToScanFor: interestingServices, peripheralCallbacks: [peripheralDiscovered], serviceCallbacks: [serviceDiscovered], valueUpdatedCallbacks: [valueUpdated])
+		return scanner
+	}
+}
 
 @main
 struct HydrometerApp: App {
-    var body: some Scene {
+	let state = AppState.shared.startBluetoothScanning()
+
+	var body: some Scene {
         WindowGroup {
             ContentView()
         }
