5c34f98e8e4fbc11df9bc9fbf591a2fee3cce427
Mike Simms <msimms01@gmail.com>
2022-08-21 14:10:52 -0400

    Backup commit.

--- Hydrometer/Hydrometer/HydrometerApp.swift
+++ Hydrometer/Hydrometer/HydrometerApp.swift
@@ -13,11 +13,57 @@ class AppState : ObservableObject {
 	static let shared = AppState()
 	@Published var currentTemp: Float = 0
 	@Published var currentGravity: Float = 0
+
+	/// Utility function for building the URL to the log file.
+	func buildLogFileUrl(location: String) throws -> URL? {
+		
+		var logDirUrl: URL?
+		
+		// Build the URL for the vault's directory. If a location was provided then
+		// use it, otherwise assume the user's iCloud directory.
+		if location.count == 0 {
+			logDirUrl = FileManager.default.url(forUbiquityContainerIdentifier: nil)
+			if logDirUrl == nil {
+				throw LogFileException.runtimeError("iCloud storage is disabled.")
+			}
+		}
+		else {
+			logDirUrl = URL(string: location)
+		}
+		logDirUrl = logDirUrl?.appendingPathComponent("Hydrometer")
+
+		// Build the URL for the vault's master file.
+		return logDirUrl?.appendingPathComponent("log.csv")
+	}
+
+	func createLogFile() {
+		do {
+			let logFileUrl = try buildLogFileUrl(location: "")
+		} catch {
+			print(error.localizedDescription)
+		}
+	}
+
+	func storeReading() {
+	}
+	
+	func convertTemperature(temperature: UInt16) -> Float {
+		0.0
+	}
+	
+	func convertGravity(gravity: UInt16) -> Float {
+		0.0
+	}
 	
 	/// Called when a peripheral is discovered.
 	/// Returns true to indicate that we should connect to this peripheral and discover its services.
 	func peripheralDiscovered(description: String) -> Bool {
-		return true
+		print(description)
+		if description.contains("Tilt") || description.contains("Hydrometer") {
+			createLogFile()
+			return true
+		}
+		return false
 	}
 	
 	/// Called when a service is discovered.
@@ -34,6 +80,11 @@ class AppState : ObservableObject {
 			serviceId == CBUUID(data: CUSTOM_BT_SERVICE_TILT_HYDROMETER6) ||
 			serviceId == CBUUID(data: CUSTOM_BT_SERVICE_TILT_HYDROMETER7) ||
 			serviceId == CBUUID(data: CUSTOM_BT_SERVICE_TILT_HYDROMETER8) {
+
+			let (rawTemperature, rawGravity) = decodeHydrometerReading(data: value)
+			self.currentTemp = convertTemperature(temperature: rawTemperature)
+			self.currentGravity = convertGravity(gravity: rawGravity)
+			storeReading()
 		}
 	}
 
---
+++ Hydrometer/Hydrometer/LogFileException.swift
@@ -0,0 +1,10 @@
+//
+//  VaultException.swift
+//  Created by Michael Simms on 8/20/22.
+//
+
+import Foundation
+
+enum LogFileException: Error {
+	case runtimeError(String)
+}
